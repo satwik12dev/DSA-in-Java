43a329b0b10572ef4ad893cc86b9749bd30b815e
Satwik Saxena <satwiksaxena41@gmail.com>
2026-01-12 13:16:45 +0530

    Dsa started

---
+++ JavaBasics/variable.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+import java.util.Scanner;
+
+public class variable{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println("Integer a = "+a);
+    }
+}
