d9f7c3e5b94fc13549ecbb39244c5b10d3ba244f
Satwik Saxena <satwiksaxena41@gmail.com>
2026-01-27 22:08:39 +0530

    Codes

--- JAVA-basics/src/main/java/Loops/aPowerb.java
+++ JAVA-basics/src/main/java/Loops/aPowerb.java
@@ -8,7 +8,6 @@ public class aPowerb {
         int n = sc.nextInt();
         int b = sc.nextInt();
         double res = Math.pow(n,b);
-
         System.out.println(res);
         int res1 = 1;
 
@@ -16,7 +15,6 @@ public class aPowerb {
         for (int i = 1; i <= b; i++) {
             res1 = res1 * n;
         }
-
         System.out.println(res1);
     }
 }
