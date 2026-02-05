2bdfdbe241eae4540139e981886e16040d35263f
Satwik Saxena <satwiksaxena41@gmail.com>
2026-02-05 22:24:42 +0530

    Sorted

---
+++ Sorting/Intersectionof2Arrays.java
@@ -0,0 +1,49 @@
+package Sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Intersectionof2Arrays {
+    public static void main(String[] args) {
+        int[] num1 = {1,2,2,1};
+        int[] num2 = {2,2};
+
+        int[] num3 = Intersection(num1,num2);
+
+        for(int ele:num3) System.out.print(ele+" ");
+    }
+
+    static int[] Intersection(int[] arr1,int[] arr2){
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int i = 0, j = 0;
+
+        while(i < arr1.length && j < arr2.length){
+            if(ans.size()>0 && ans.get(ans.size()-1)==Math.min(arr1[i],arr2[j])){
+                if(arr1[i]==arr2[j]){
+                    i++;
+                    j++;
+                }
+                else if (arr1[i]<arr2[j]) i++;
+                else if (arr1[i]>arr2[j]) j++;
+                continue;
+            }
+
+            if(arr1[i]==arr2[j]){
+                ans.add(arr1[i]);
+                i++;
+                j++;
+            }
+            else if (arr1[i]<arr2[j]) i++;
+            else if (arr1[i]>arr2[j]) j++;
+        }
+        int[] result = new int[ans.size()];
+        for (int k = 0; k < ans.size(); k++) {
+            result[k] = ans.get(k);
+        }
+
+        return result;
+    }
+}
---
+++ Sorting/IntersectionofTwoSortedArrays.java
@@ -0,0 +1,45 @@
+package Sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class IntersectionofTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 2, 3};
+        int[] arr2 = {2,2,3};
+        ArrayList<Integer> result = IntersectionofSortedArray(arr1,arr2);
+
+        System.out.println(result);
+    }
+    
+    static ArrayList<Integer> IntersectionofSortedArray(int[] arr1, int[] arr2){
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int i = 0, j = 0;
+
+        while(i < arr1.length && j < arr2.length){
+            if(ans.size()>0 && ans.get(ans.size()-1)==Math.min(arr1[i],arr2[j])){
+                if(arr1[i]==arr2[j]){
+                    i++;
+                    j++;
+                }
+                else if (arr1[i]<arr2[j]) i++;
+                else if (arr1[i]>arr2[j]) j++;
+                continue;
+            }
+
+            if(arr1[i]==arr2[j]){
+                ans.add(arr1[i]);
+                i++;
+                j++;
+            }
+            else if (arr1[i]<arr2[j]) i++;
+            else if (arr1[i]>arr2[j]) j++;
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
